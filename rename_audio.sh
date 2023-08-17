761817dc2da9b7d741538411c3396c0208aa4103
Christian Huber <christian.huber@kit.edu>
2023-08-17 11:18:35 +0200

    Removed spaces in file names

--- rename_audio.sh
+++ rename_audio.sh
@@ -2,7 +2,7 @@
 mkdir -p audio
 for f in *.opus
 do
-    f2=`echo $f | cut -d"-" -f1 | sed "s/｜/-/g" | cut -d"-" -f1`
+    f2=`echo $f | cut -d"-" -f1 | sed "s/｜/-/g" | cut -d"-" -f1 | sed "s/ /_/g"`
     f2=${f2::-1}".opus"
     mv "$f" audio/"$f2"
 done
